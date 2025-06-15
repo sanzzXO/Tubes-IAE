eb7477e5809e96e4927c748e3d8b15725abe60cc
amiertia <ertiaami@gmail.com>
2025-06-15 23:18:42 +0700

    feat: add controller logic for borrowing books, ready for test

--- borrowing-service/routes/api.php
+++ borrowing-service/routes/api.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BorrowingController;
 
 Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
-    // Borrowing routes
-    Route::apiResource('borrowings', BorrowingController::class);
-    Route::post('borrowings/{id}/return', [BorrowingController::class, 'returnBook']);
-    Route::post('borrowings/{id}/extend', [BorrowingController::class, 'extend']);
-    Route::get('users/{userId}/borrowings', [BorrowingController::class, 'userHistory']);
-    Route::get('borrowings-statistics', [BorrowingController::class, 'statistics']);
+    // // Borrowing routes
+    // Route::apiResource('borrowings', BorrowingController::class);
+    // Route::post('borrowings/{id}/return', [BorrowingController::class, 'returnBook']);
+    // Route::post('borrowings/{id}/extend', [BorrowingController::class, 'extend']);
+    // Route::get('users/{userId}/borrowings', [BorrowingController::class, 'userHistory']);
+    // Route::get('borrowings-statistics', [BorrowingController::class, 'statistics']);
+    Route::post('/borrowings', [BorrowingController::class, 'borrow']);
+    Route::post('/borrowings/return/{id}', [BorrowingController::class, 'return']);
+    Route::post('/borrowings/extend/{id}', [BorrowingController::class, 'extend']);
+
 });
