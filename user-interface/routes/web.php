0038e87d00f0da3e02b42874fba1ae95e0dac606
sanzzXO <ihsannafil36@gmail.com>
2025-06-20 22:32:24 +0700

    removing

--- user-interface/routes/web.php
+++ user-interface/routes/web.php
@@ -34,33 +34,3 @@ Route::middleware('auth.session')->group(function () {
     Route::post('/reviews', [ReviewController::class, 'store']);
 });
 
-Route::prefix('staff')->group(function () {
-    Route::get('/dashboard', 'App\\Http\\Controllers\\StaffController@dashboard');
-    // Buku
-    Route::get('/books', 'App\\Http\\Controllers\\StaffBookController@index');
-    Route::get('/books/create', 'App\\Http\\Controllers\\StaffBookController@create');
-    Route::post('/books', 'App\\Http\\Controllers\\StaffBookController@store');
-    Route::get('/books/{id}/edit', 'App\\Http\\Controllers\\StaffBookController@edit');
-    Route::post('/books/{id}/update', 'App\\Http\\Controllers\\StaffBookController@update');
-    Route::post('/books/{id}/delete', 'App\\Http\\Controllers\\StaffBookController@destroy');
-    // Kategori
-    Route::get('/categories', 'App\\Http\\Controllers\\StaffCategoryController@index');
-    Route::get('/categories/create', 'App\\Http\\Controllers\\StaffCategoryController@create');
-    Route::post('/categories', 'App\\Http\\Controllers\\StaffCategoryController@store');
-    Route::get('/categories/{id}/edit', 'App\\Http\\Controllers\\StaffCategoryController@edit');
-    Route::post('/categories/{id}/update', 'App\\Http\\Controllers\\StaffCategoryController@update');
-    Route::post('/categories/{id}/delete', 'App\\Http\\Controllers\\StaffCategoryController@destroy');
-    // User
-    Route::get('/users', 'App\\Http\\Controllers\\StaffUserController@index');
-    Route::get('/users/create', 'App\\Http\\Controllers\\StaffUserController@create');
-    Route::post('/users', 'App\\Http\\Controllers\\StaffUserController@store');
-    Route::get('/users/{id}/edit', 'App\\Http\\Controllers\\StaffUserController@edit');
-    Route::post('/users/{id}/update', 'App\\Http\\Controllers\\StaffUserController@update');
-    Route::post('/users/{id}/delete', 'App\\Http\\Controllers\\StaffUserController@destroy');
-    // Borrowing
-    Route::get('/borrowings', 'App\\Http\\Controllers\\StaffBorrowingController@index');
-    Route::post('/borrowings/{id}/return', 'App\\Http\\Controllers\\StaffBorrowingController@return');
-    // Review
-    Route::get('/reviews', 'App\\Http\\Controllers\\StaffReviewController@index');
-    Route::post('/reviews/{id}/delete', 'App\\Http\\Controllers\\StaffReviewController@destroy');
-});
